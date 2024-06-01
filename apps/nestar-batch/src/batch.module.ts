7bd6439fac95ab34291c5f8a791c76915e1698e8
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-06-01 17:09:25 +0900

    feat: develop job scheduler business logic

--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -4,11 +4,18 @@ import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import MemberSchema from 'apps/nestar-api/src/schemas/Member.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import PropertySchema from 'apps/nestar-api/src/schemas/Property.model';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    DatabaseModule, ScheduleModule.forRoot()
+    DatabaseModule, 
+    ScheduleModule.forRoot(),
+    MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+    MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+
   ],
   controllers: [BatchController],
   providers: [BatchService],
